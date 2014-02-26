487cccee7cff9e3f644007365f75745ce07f9708
Sebastian Vetter <sebastian@roadside-developer.com>
2014-02-26 11:27:03 +1100

    Create new fabric command that allows running management commands

--- fluffy/django.py
+++ fluffy/django.py
@@ -6,17 +6,21 @@ from .output import notify
 from .remote import venv_sudo
 
 
+def run_manage(command):
+    with cd(env.code_dir):
+        venv_sudo('./manage.py {0}'.format(command))
+
+
 def run_offline_compressor():
     """ Generate minified CSS and JS as well as pre-compile LESS files. """
     notify('Running offline compression')
-    with cd(env.code_dir):
-        venv_sudo('./manage.py compress --follow-links > /dev/null')
+    run_manage('compress --follow-links > /dev/null')
 
 
 def collect_static_files():
     notify("Collecting static files")
     with cd(env.code_dir):
-        venv_sudo('./manage.py collectstatic --noinput > /dev/null')
+        run_manage('collectstatic --noinput > /dev/null')
         sudo('chmod -R g+w public')
 
 
@@ -26,6 +30,5 @@ def migrate():
     Apply any schema alterations
     """
     notify("Applying database migrations")
-    with cd(env.code_dir):
-        venv_sudo('./manage.py syncdb --noinput > /dev/null')
-        venv_sudo('./manage.py migrate --ignore-ghost-migrations')
+    run_manage('syncdb --noinput > /dev/null')
+    run_manage('migrate --ignore-ghost-migrations')
