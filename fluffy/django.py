f2f35295eeb03f209338752d704dbcbbbd2c2018
Sebastian Vetter <sebastian@roadsi.de>
2014-07-02 16:24:46 +1000

    Add function to get current Django version

--- fluffy/django.py
+++ fluffy/django.py
@@ -11,6 +11,11 @@ def run_manage(command):
         venv_sudo('./manage.py {0}'.format(command))
 
 
+def _get_django_version():
+    import django
+    return django.VERSION
+
+
 def run_offline_compressor():
     """ Generate minified CSS and JS as well as pre-compile LESS files. """
     notify('Running offline compression')
@@ -26,9 +31,14 @@ def collect_static_files():
 
 @runs_once
 def migrate():
-    """
-    Apply any schema alterations
-    """
-    notify("Applying database migrations")
-    run_manage('syncdb --noinput > /dev/null')
-    run_manage('migrate --ignore-ghost-migrations')
+    """ Apply schema migrations based on the Django version used.  """
+    notify("Applying database migrations for Django {}".format(
+        env.django_version))
+
+    if env.django_version[:3] < (1, 7):
+        # Using the command for South migrations
+        run_manage('syncdb --noinput > /dev/null')
+        run_manage('migrate --ignore-ghost-migrations')
+    else:
+        # Using the command for Django's migrations (Django 1.7+)
+        run_manage('migrate --noinput')
